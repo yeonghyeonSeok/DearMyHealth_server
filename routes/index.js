d9e294a22ce59f87da48d7086aa5f008e705502d
hhiyeon <hhyyeon0214@gmail.com>
2019-09-18 19:14:32 +0900

    commit router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+//router.use("/auth", require("./auth/index"));
+/*
+router.use("/main", require("./main/index"));
+router.use("/review", require("./review/index"));
+router.use("/course", require("./course/index"));
+router.use("/place", require("./place/index"));
+router.use("/mypage", require("./mypage/index"));
+*/
+//router.use("/set", require("./set/index"));
 
 module.exports = router;
