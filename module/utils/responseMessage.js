a7ab0bf422161debefa4bc34299fd7fcdec87bd4
hhiyeon <hhyyeon0214@gmail.com>
2019-09-19 02:06:02 +0900

    commit resMessage#2

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -4,12 +4,14 @@ module.exports = {
     OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
 //////////////////////////////////////////// 추가
-
+    /* AUTH */
+    NOT_CORRECT_INFO: "올바르지 않은 정보 입니다 ",
+    SUCCESS_FIND_PASSWORD: "비밀번호 찾기 성공",
 
     /* SET */
     SUCCESS_USER_LIST: "회원정보 조회 성공",
     SUCCESS_UPDATE_PASSWORD: "비밀번호 수정 성공",
-    NOT_CORRECT_PASSWORD: "현재 비밀번호가 일치하지 않습니다.",
+    NOT_CORRECT_PASSWORD: "현재 비밀번호가 일치하지 않습니다",
     SUCCESS_UPDATE_NICKNAME: "닉네임 수정 성공",
     SUCCESS_SENT_EMAIL: "이메일 전송 성공", 
 };
