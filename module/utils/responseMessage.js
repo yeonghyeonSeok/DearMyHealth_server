2e6d7b3e8c6de1e07d16b79319e946667831fc9e
hhiyeon <hhyyeon0214@gmail.com>
2019-09-18 20:25:58 +0900

    commit resMessage#1

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -5,4 +5,11 @@ module.exports = {
 
 //////////////////////////////////////////// 추가
 
+
+    /* SET */
+    SUCCESS_USER_LIST: "회원정보 조회 성공",
+    SUCCESS_UPDATE_PASSWORD: "비밀번호 수정 성공",
+    NOT_CORRECT_PASSWORD: "현재 비밀번호가 일치하지 않습니다.",
+    SUCCESS_UPDATE_NICKNAME: "닉네임 수정 성공",
+    SUCCESS_SENT_EMAIL: "이메일 전송 성공", 
 };
