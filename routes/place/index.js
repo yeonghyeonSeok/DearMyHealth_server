4ff410d0aa30d5e1131576c6f44d65179926b5b5
yeonghyeonSeok <syh4834@naver.com>
2019-09-21 05:54:39 +0900

    place search

--- routes/place/index.js
+++ routes/place/index.js
@@ -5,5 +5,6 @@ var router = express.Router({mergeParams: true});
 router.use("/info", require("./info"));
 router.use("/like", require("./like"));
 router.use("/enroll", require("./enroll"));
+router.use("/search", require("./search"));
 
 module.exports = router;
