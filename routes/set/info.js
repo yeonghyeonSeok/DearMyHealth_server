f41d5ef9163135ff32f3ce9aefb6438a1d368d4f
hhiyeon <hhyyeon0214@gmail.com>
2019-09-23 03:44:04 +0900

    commit set#3

--- routes/set/info.js
+++ routes/set/info.js
@@ -12,14 +12,14 @@ const db = require('../../module/pool');
 // 회원정보 조회
 router.get('/', authUtil.isLoggedin, async (req, res) => {
     const infoSelectQuery = 'SELECT email, nickname FROM user WHERE userIdx = ?'; 
-    const infoSelectResult = await db.queryParam_Arr(infoSelectQuery, [req.decoded.idx]);
+    const infoSelectResult = await db.queryParam_Arr(infoSelectQuery, [req.decoded.userIdx]);
 
+    console.log(infoSelectResult);
     if(!infoSelectResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // DB 오류
     }else{
         res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_USER_LIST, infoSelectResult));      // 회원정보 조회 성공
     }
-    console.log(infoSelectResult);
 });
 
 module.exports = router;
--- routes/set/modify.js
+++ routes/set/modify.js
@@ -11,9 +11,10 @@ const db = require('../../module/pool');
 
 // 회원정보 수정 - 비밀번호
 router.put('/password', authUtil.isLoggedin, async (req, res) => {
-    const pwdSelectQuery = 'SELECT password FROM user WHERE userIdx = ?'
-    const pwdSelectResult = await db.queryParam_Arr(pwdSelectQuery, [req.decoded.idx]);
+    const pwdSelectQuery = 'SELECT * FROM user WHERE userIdx = ?'
+    const pwdSelectResult = await db.queryParam_Arr(pwdSelectQuery, [req.decoded.userIdx]);
     
+    console.log(pwdSelectResult);
     if(!pwdSelectResult){  
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));  // DB 에러
     } else{  
@@ -22,13 +23,13 @@ router.put('/password', authUtil.isLoggedin, async (req, res) => {
         // 현재 비밀번호
 
         // pwdSelectResult[0] 해당 유저의 pw와 hashed Pw가 동일할 때 = 비밀번호 일치
-        if(pwdSelectResult[0].user_pw == hashedCurrentPw.toString('base64')){   
+        if(pwdSelectResult[0].password == hashedCurrentPw.toString('base64')){   
 
             // 새 비밀번호로 수정
             const hashedNewPw = await crypto.pbkdf2(req.body.new_pwd.toString(), salt, 1000, 32, 'SHA512');
             const pwdUpdateQuery = 'UPDATE user SET password = ? WHERE userIdx = ?';
             const pwdUpdateResult = await db.queryParam_Arr(pwdUpdateQuery, 
-                [req.body.password, hashedNewPw.toString('base64'), req.decoded.idx]);
+                [hashedNewPw.toString('base64'), req.decoded.userIdx]);
 
                 if(!pwdUpdateResult){
                     res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR)); // DB  에러
@@ -43,15 +44,8 @@ router.put('/password', authUtil.isLoggedin, async (req, res) => {
 
 // 회원정보 수정 - 닉네임
 router.put('/nickname', authUtil.isLoggedin, async (req, res) => {
-    // const nameSelectQuery = 'SELECT nickname FROM user WHERE userIdx = ?'
-    // const nameSelectResult = await db.queryParam_Arr(nameSelectQuery, [req.decoded.idx]);
-    
-    // if(!nameSelectResult){  
-    //     res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));  // DB 에러
-    // } else {
-    // 새로운 닉네임 수정
-        const nameUpdateQuery = 'UPDATE user SET nickname WHERE userIdx = ?';
-        const nameUpdateResult = await db.queryParam_Arr(nameUpdateQuery,[req.body.new_name, req.decoded.idx]);
+    const nameUpdateQuery = 'UPDATE user SET nickname = ? WHERE userIdx = ?';
+    const nameUpdateResult = await db.queryParam_Arr(nameUpdateQuery,[req.body.new_name, req.decoded.userIdx]);
 
     if(!nameUpdateResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR)); // DB  에러
