16ca58765739b711eb2b56f2c6854e254365235d
yeonghyeonSeok <syh4834@naver.com>
2019-09-22 21:39:30 +0900

    tagSearch

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -18,6 +18,8 @@ module.exports = {
     SUCCESS_FIND_PASSWORD: "비밀번호 찾기 성공",
 
     /* COURSE */
+    TAG_SEARCH_SUCCESS : "태그 검색 성공",
+
     ALREADY_LIKE_COURSE: "이미 좋아요 상태인 코스",
     FAIL_LIKE_COURSE: "코스 좋아요 실패",
     SUCCESS_LIKE_COURSE: "코스 좋아요 성공",
