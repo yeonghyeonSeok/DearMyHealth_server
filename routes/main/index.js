f9023601c37d7fce4b44318bd6b73610878a522b
hhiyeon <hhyyeon0214@gmail.com>
2019-09-24 06:20:39 +0900

    commit main-index

--- routes/main/index.js
+++ routes/main/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.use("/search", require("./search"));
+router.use("/search", require("./search")); // 메인 뷰 검색
+router.use("/order", require("./order"));   // 인기순 코스 조회
+router.use("/list", require("./list")); // 코스 타입별 전체조회 - 0 오래가게 1 한국전통 2 사용자
 
 module.exports = router;
