a5b25a70e7177f5bff2eeabb7a4381e947856a60
hhiyeon <hhyyeon0214@gmail.com>
2019-09-25 03:11:06 +0900

    commit mypage-myReview

--- routes/mypage/myReview.js
+++ routes/mypage/myReview.js
@@ -11,27 +11,40 @@ const db = require('../../module/pool');
 // 내가 남긴 리뷰 조회
 router.get('/', authUtil.isLoggedin, async (req, res) => {
     let resAllData = [];
-    // const edtSelectQuery = 'SELECT * FROM user JOIN course ON user.userIdx = course.userIdx WHERE userIdx = ? ORDER BY course.cLikeCount DESC'; 
-    const edtSelectQuery = 'SELECT course.courseIdx , course.cName, course.cThumbnail, cLikeCount'+
-    ' FROM user JOIN course ON user.userIdx = course.userIdx WHERE user.userIdx = ? ORDER BY course.cLikeCount DESC'; 
-    const edtSelectResult = await db.queryParam_Arr(edtSelectQuery, [req.decoded.userIdx]);
+    const courseSelectQuery = 'SELECT course.courseIdx, course.cThumbnail, course.cName, course.cLikeCount , course.cReviewCount, review.createdAt , review.comment, review.emotion'+
+    ' FROM course JOIN review ON course.courseIdx = review.courseIdx WHERE review.userIdx = ? ORDER BY course.cLikeCount DESC';
+    // const courseSelectQuery = 'SELECT *'+
+    // ' FROM course JOIN review ON course.courseIdx = review.courseIdx WHERE review.userIdx = ? ORDER BY course.cLikeCount DESC';
+    const courseSelectResult = await db.queryParam_Arr(courseSelectQuery, [req.decoded.userIdx]);
 
-    console.log(edtSelectResult);
-    if(!edtSelectResult){
+    // const placeSelectQuery = 'SELECT *'+
+    // ' FROM place JOIN review ON place.placeIdx = review.placeIdx WHERE review.userIdx = ? ORDER BY place.place_like DESC';
+    const placeSelectQuery = 'SELECT place.placeIdx, place.place_thumbnail, place.pName, place.place_like, place.pReviewCount, review.createdAt, review.comment, review.emotion'+
+    ' FROM place JOIN review ON place.placeIdx = review.placeIdx WHERE review.userIdx = ? ORDER BY place.place_like DESC';
+    const placeSelectResult = await db.queryParam_Arr(placeSelectQuery, [req.decoded.userIdx]);
+
+
+    for (let i = 0; i < courseSelectResult.length; i++) {
+        const item = {
+            info: []
+        }
+        item.info.push(courseSelectResult[i]);
+        resAllData.push(item);
+    }
+
+    for (let i = 0; i < placeSelectResult.length; i++) {
+        const item = {
+            info: []
+        }
+        item.info.push(placeSelectResult[i]);
+        resAllData.push(item);
+    }
+    resAllData.sort(function(a, b) {return b-a});
+
+    if(!courseSelectResult && !placeSelectResult){
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));     // DB 오류
     }else{
-        if(edtSelectResult[0] != null){ // 내가 만든 코스가 존재할 경우
-            for (let i = 0; i < edtSelectResult.length; i++) {
-                const item = {
-                    info: []
-                }
-                item.info.push(edtSelectResult[i]);
-                resAllData.push(item);
-            }
-            res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_EDIT_COURSE, resAllData));      // 내가 만든 코스 조회성공
-        } else {    // 내가 만든 코스가 없는 경우
-            res.status(200).send(defaultRes.successFalse(statusCode.OK, resMessage.NOT_EDIT_COURSE));  // 내가 만든 코스가 없습니다
-        }
+        res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.SUCCESS_MY_REVIEW, resAllData));      // 내가 남긴 리뷰 조회성공
     }
 });
 
